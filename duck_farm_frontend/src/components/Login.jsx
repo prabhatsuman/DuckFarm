e7e0b9461f5bad66510e880c7c7cf256bf40b7c2
prabhatsuman <mr.prabhatsuman@gmail.com>
2024-08-07 01:56:35 +0530

    login page modified

--- duck_farm_frontend/src/components/Login.jsx
+++ duck_farm_frontend/src/components/Login.jsx
@@ -6,6 +6,7 @@ import API_URL from "../config";
 const Login = ({ toggleComponent }) => {
   const navigate = useNavigate();
   const [loginState, setLoginState] = useState({ email: "", password: "" });
+  const [errorMessage, setErrorMessage] = useState(""); // State for error message
 
   const handleChange = (e) => {
     setLoginState({ ...loginState, [e.target.name]: e.target.value });
@@ -33,64 +34,71 @@ const Login = ({ toggleComponent }) => {
       })
       .catch((error) => {
         console.error("Login failed:", error);
+        if (error.response && error.response.data) {
+          setErrorMessage(error.response.data.detail); // Set error message from backend
+        } else {
+          setErrorMessage("An unknown error occurred."); // Set a generic error message
+        }
       });
   };
 
   return (
-   
-      <div className="bg-opacity-60 rounded-lg bg-blue-950 py-8 px-4 shadow w-1/2">
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <div className="mt-1">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Enter your email"
-                value={loginState.email}
-                onChange={handleChange}
-              />
-            </div>
+    <div className="bg-opacity-60 rounded-lg bg-blue-950 py-8 px-4 shadow w-1/2">
+      {errorMessage && (
+        <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
+          {errorMessage}
+        </div>
+      )}
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div>
+          <div className="mt-1">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Enter your email"
+              value={loginState.email}
+              onChange={handleChange}
+            />
           </div>
+        </div>
 
-          <div>
-            <div className="mt-1">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Enter your password"
-                value={loginState.password}
-                onChange={handleChange}
-              />
-            </div>
+        <div>
+          <div className="mt-1">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Enter your password"
+              value={loginState.password}
+              onChange={handleChange}
+            />
           </div>
+        </div>
 
-          
-          <div className="flex flex-col gap-7">
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-950 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Log in
-            </button>
-            <button
-              type="button"
-              onClick={toggleComponent}
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-700 hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Create an account
-            </button>
-          </div>
-        </form>
-      </div>
-    
+        <div className="flex flex-col gap-7">
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-950 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Log in
+          </button>
+          <button
+            type="button"
+            onClick={toggleComponent}
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-700 hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Create an account
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
